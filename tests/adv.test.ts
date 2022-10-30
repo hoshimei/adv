3412049dc075a182777895f4811e8a6c28b05432
Outvi V <git@outv.im>
2022-10-30 14:18:40 +0800

    feat: get a full e2e test

--- tests/adv.test.ts
+++ tests/adv.test.ts
@@ -1,12 +1,16 @@
 import { readFileSync } from 'node:fs'
 import { expect, test } from 'vitest'
 import dirname from '../src/utils/dirname'
-import { parse } from '../src/index'
+import { parse, read } from '../src/index'
 import path from 'node:path'
 
 const __dirname = dirname(import.meta.url)
+const str = readFileSync(path.join(__dirname, './raw/adv.txt'), 'utf-8')
 
 test('parse the full env', () => {
-  const str = readFileSync(path.join(__dirname, './raw/adv.txt'), 'utf-8')
   expect(parse(str)).toMatchSnapshot()
+})
+
+test('read the full env', () => {
+  expect(read(str)).toMatchSnapshot()
 })
