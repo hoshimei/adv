d240fa5578de701074d4315d02c53abd1dc7bf81
Outvi V <git@outv.im>
2022-11-12 00:50:46 +0800

    fix(message): name can be void

--- src/parts/message.ts
+++ src/parts/message.ts
@@ -6,7 +6,8 @@ export default function read(t: RawCommand): Message {
   return {
     _t: 'Message',
     text: pickFirst(t, 'text', 'string'),
-    name: pickFirst(t, 'name', 'string'),
+    // QA had an null name at adv_event_2209_01_01.txt.
+    name: tryPickFirst(t, 'name', 'string') ?? '',
     thumbnail: tryPickFirst(
       t,
       'thumbnial', // QA English!
