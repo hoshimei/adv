5a4c8e8ccf0d0cffafd55be4fc88eb66a963a12c
Outvi V <git@outv.im>
2022-11-14 01:03:55 +0800

    ci: observe_full

---
+++ observer/full.mjs
@@ -0,0 +1,27 @@
+import * as adv from '../dist/index.js'
+import { getAllStoriesOcto, putFile } from './utils.mjs'
+
+// Main routine
+;(async () => {
+  const version = (
+    await import('../package.json', { assert: { type: 'json' } })
+  ).default.version
+  const stories = await getAllStoriesOcto()
+  await Promise.all(
+    stories.map(({ name, objectName, generation }) => {
+      const savePath = `processed/adv/${name}.json`
+      return (async () => {
+        const storyText = await fetch(
+          `https://${process.env.UPSTREAM_BASE}/${objectName}?generation=${generation}&alt=media`
+        ).then((x) => x.text())
+        const parsed = adv.read(storyText)
+        await putFile(savePath, JSON.stringify({ v: version, l: parsed }))
+      })()
+        .then(() => console.log(`Finished: ${savePath}`))
+        .catch((e) => {
+          console.warn(`Error: ${savePath} [${e}]`)
+          throw e
+        })
+    })
+  )
+})()
---
+++ observer/utils.mjs
@@ -0,0 +1,30 @@
+import { S3 } from '@aws-sdk/client-s3'
+
+const bucketName = process.env.ASSETS_BUCKET_NAME
+const s3 = new S3({
+  endpoint: process.env.S3_ENDPOINT,
+  region: process.env.S3_REGION,
+  credentials: {
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  },
+})
+
+export function getAllStoriesOcto() {
+  return fetch('https://idoly-backend.outv.im/manage/raw?key=Octo', {
+    headers: {
+      Authorization: `Bearer ${process.env.BACKEND_MANAGE_READ_KEY}`,
+    },
+  })
+    .then((x) => x.json())
+    .then((x) => x.resourceList.filter((y) => y.name.endsWith('txt')))
+}
+
+export function putFile(savePath, body) {
+  return s3.putObject({
+    Bucket: bucketName,
+    Key: savePath,
+    Body: body,
+    ContentType: 'application/json;charset=utf-8',
+  })
+}
